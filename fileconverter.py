6dc0f8b1bfbff620a3eab7f7ff9828302785fd03
paulopperman <paul.opperman@gmail.com>
2017-04-03 23:51:34 -0400

    * add geometry conversion function in mapfile_tools
    * upload new test output with lon-lat geometry

--- fileconverter.py
+++ fileconverter.py
@@ -3,18 +3,35 @@ Convert shapefiles to GeoJSON for easy mapping on GitHub
 
 based on https://gist.github.com/frankrowe/6071443
 
+RI Projection data is in on NAD 1983 StatePlane RI FIPS 3800 Feet.  EPSG 102730
+Use pyproj to transform, and don't forget to set preserve_units=True! https://github.com/jswhit/pyproj/issues/67
+
++proj=tmerc +lat_0=41.08333333333334 +lon_0=-71.5 +k=0.99999375 +x_0=100000 +y_0=0 +datum=NAD83 +units=us-ft +no_defs
+
+Data sources:
+RIPTA - RIPTA routes http://www.rigis.org/data/RIPTAroutes
+LUSTS - Leaky underground storage tanks http://www.rigis.org/data/LUSTs
 """
 
 import shapefile
-from localfiles import SHPPATH
+from pyproj import Proj
+from mapfile_tools import change_proj
 
-reader = shapefile.Reader(SHPPATH)
+# import path strings to the location of the shape data
+from localfiles import RIPTA, LUSTS
+
+# define projection to convert to lon-lat
+NAD_1983_RI_string = '+proj=tmerc +lat_0=41.08333333333334 +lon_0=-71.5 +k=0.99999375 +x_0=100000 +y_0=0 +datum=NAD83 +units=us-ft +no_defs'
+# ri_state = Proj( NAD_1983_RI_string, preserve_units=True)
+
+reader = shapefile.Reader(RIPTA)
 fields = reader.fields[1:]
 field_names = [field[0] for field in fields]
 buffer = []
 for sr in reader.shapeRecords():
     atr = dict(zip(field_names, sr.record))
     geom = sr.shape.__geo_interface__
+    geom = change_proj(geom,NAD_1983_RI_string)
     buffer.append(dict(type="Feature", geometry=geom, properties=atr))
 
 # insert for python 3 compatibility - https://groups.google.com/forum/#!topic/geospatialpython/7bZnpHkD7ys
@@ -22,6 +39,6 @@ buffer = str(buffer)
 # write the GeoJSON file
 from json import dumps
 
-geojson = open("pyshp-demo.json", "w")
+geojson = open("riptaroutes-test2.geojson", "w")
 geojson.write(dumps({"type": "FeatureCollection", "features": buffer}, indent=2) + "\n")
 geojson.close()
---
+++ mapfile_tools.py
@@ -0,0 +1,27 @@
+"""
+Some home-rolled utility functions for working with shapefiles
+"""
+from pyproj import Proj
+
+
+def change_proj(geom, projection):
+    # convert geojson geometry coordinates from projection to lat lon
+    # projection is a proj4 string
+    p = Proj(projection, preserve_units=True)
+    geo_type = geom['type'] # get the type of geometry to allow for proper iterating
+    if geo_type == 'Point':
+        x,y = geom['coordinates']
+        lon, lat = p(x, y, inverse=True)
+        c = tuple([lon,lat])
+    else:  # this currently is tested for a MultiLineString
+        buffer = []
+        for part in geom['coordinates']:
+            x, y = list(zip(*list(part)))
+            lons, lats = p(x, y, inverse=True)
+            line_seg_coords = tuple(zip(lons, lats))
+            buffer.append(line_seg_coords)
+        c = tuple(buffer)
+
+    new_geom = {'type':geo_type,'coordinates':c}
+
+    return new_geom
